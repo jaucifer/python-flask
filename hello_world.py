85833a1da66d1088decc71040967ce658ef0f83f
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-09 12:30:00 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,17 +2,7 @@ from flask import Flask
 
 def wrap_html(greet):
     html = """
-        <html>
-        <body>
-            <div style='font-size:80px;'>
-            <center>
-                <image height="600" width="531" src="https://secure.meetupstatic.com/photos/event/2/a/a/3/600_452110915.jpeg">
-                <br>
-                {0}<br>
-            </center>
-            </div>
-        </body>
-        </html>""".format(greet)
+        """.format(greet)
     return html
 
 app = Flask(__name__)
