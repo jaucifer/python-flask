5834045193d6951f1011d2833ab8cbb32b38fccd
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-09 12:44:21 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def wrap_html(greet):
-    html = format(greet)
+    html = .format(greet)
     return html
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     greet = 'Welcome to CI/CD'
+    html = wrap_html(greet)
     return html
 
 if __name__ == '__main__':
