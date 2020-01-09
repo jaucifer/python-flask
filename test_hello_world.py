665faa94ae3e31a5131d626e96ebae9c82f3e0d9
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-09 12:16:47 +0800

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -2,12 +2,12 @@ import hello_world
 import unittest
 
 class TestHelloWorld(unittest.TestCase):
-  def setup(self):
-    self.app = hello_world.app.test_client()
-    self.app.testing = True
+  def setUp(self):
+        self.app = hello_world.app.test_client()
+        self.app.testing = True
   def test_status_code(self):
-    response = self.app.get('/')
-    self.assertEqual(response.status_code, 200)
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
   def test_greeting_message(self):
     response = self.app.get('/')
     greet = hello_world.wrap_html('Welcome to CI/CD')
